1cf384154bc199f7c601c6624ac2d38697070d6d
gaosx <gsx0312@qq.com>
2023-08-06 13:51:33 +0800

    feat: 以下是测试用例代码，用于验证当用户成功接入 WebSocket 后，如果有其他用户回复了其发表的帖子评论，用户将收到未读通知数推送的情况：

--- tests/socket/notifications.test.js
+++ tests/socket/notifications.test.js
@@ -562,6 +562,74 @@ describe('Notifications Socket', () => {
         });
     });
 
+    it('should receive unread notifications count via WebSocket after receiving a reply to a comment', (done) => {
+        // 创建带有认证信息的 WebSocket 连接
+        const socket = ioClient(`http://localhost:${config.port}`, {
+            auth: {
+                token: token,
+            },
+        });
+
+        // 标记是否已经接收到第一个未读通知数消息
+        let firstUnreadMessageReceived = false;
+
+        // 监听连接成功事件
+        socket.on('connect', () => {
+            // 监听 WebSocket 推送消息
+            socket.on('notifications', (message) => {
+                // 只处理未读通知数消息
+                if (message.type !== 2) {
+                    return;
+                }
+
+                // 忽略第一个消息
+                if (!firstUnreadMessageReceived) {
+                    firstUnreadMessageReceived = true;
+                    return;
+                }
+
+                // 对推送的消息进行断言
+                chai.expect(message).to.deep.equal({
+                    type: 2,
+                    data: {
+                        count: 1,
+                    },
+                });
+
+                done();
+            });
+
+            // 在连接成功后，调用评论帖子的接口
+            chai.request(app)
+                .post(`/posts/${postId}/comment`)
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    content: '这是一条评论',
+                })
+                .end((commentErr, commentRes) => {
+                    if (commentErr) {
+                        done(commentErr);
+                    }
+
+                    const commentId = commentRes.body.commentId;
+
+                    // 使用获取到的评论ID调用回复评论的接口
+                    chai.request(app)
+                        .post(`/posts/${postId}/comment`)
+                        .set('Authorization', `Bearer ${otherToken}`)
+                        .send({
+                            content: '这是一条回复评论',
+                            parentId: commentId, // 将评论的 ID 作为父级评论 ID
+                        })
+                        .end((replyErr) => {
+                            if (replyErr) {
+                                done(replyErr);
+                            }
+                        });
+                });
+        });
+    });
+
     afterEach(async () => {
         // 关闭 WebSocket 连接
         if (socket.connected) {
