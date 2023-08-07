e91eb8ea943ff874cae0464062c0110297512e22
gaosx <gsx0312@qq.com>
2023-08-07 21:16:41 +0800

    feat: 测试客户读向服务端推送标记消息已读

--- tests/socket/messages.test.js
+++ tests/socket/messages.test.js
@@ -688,6 +688,173 @@ describe('Messages Socket', () => {
         });
     });
 
+    describe('Mark Message As Read', () => {
+        it('should receive marked-read message notification on opponent user', (done) => {
+            // 会话id
+            let conversationId;
+
+            // 消息id
+            let messageId;
+
+            // 最后一条消息的发送时间
+            let lastMessageTime;
+
+            // 最后一条消息的内容
+            let lastMessageContent = 'Hello';
+
+            // 创建标志变量来跟踪断言状态
+            let socketAssertion = false;
+            let otherSocketAssertion = false;
+
+            // 创建带有认证信息的 WebSocket 连接
+            socket = ioClient(`http://localhost:${config.port}`, {
+                auth: {
+                    token: token
+                }
+            });
+
+            // 创建带有其他用户认证信息的 WebSocket 连接
+            otherSocket = ioClient(`http://localhost:${config.port}`, {
+                auth: {
+                    token: otherToken
+                }
+            });
+
+            // 监听连接成功事件
+            socket.on('connect', () => {
+                // 监听 WebSocket 推送消息
+                socket.on('notifications', (message) => {
+                    // 如果是聊天会话对象消息
+                    if (message.type === 3) {
+                        // 验证会话对象的结构和属性
+                        const conversation = message.data;
+                        chai.expect(conversation).to.have.property('conversationId');
+
+                        // 验证会话对象的属性值
+                        chai.expect(conversation.conversationId).to.be.a('string');
+
+                        // 获取会话id
+                        conversationId = conversation.conversationId;
+
+                        // 客户端推送发送消息到服务端
+                        socket.emit('messages', {
+                            type: 4,
+                            data: {
+                                conversationId,
+                                opponentUserId: otherUser.id,
+                                content: lastMessageContent
+                            },
+                        });
+                    }
+
+                    // 如果是已标记为已读的消息对象
+                    if (message.type === 8) {
+                        // 验证消息的结构和属性
+                        const messageData = message.data;
+
+                        // 验证消息对象的结构和属性
+                        chai.expect(messageData).to.have.property('conversationId');
+                        chai.expect(messageData).to.have.property('messageId');
+                        chai.expect(messageData).to.have.property('readStatus');
+
+                        // 验证消息对象的属性值
+                        chai.expect(messageData.conversationId).to.be.a('string');
+                        chai.expect(messageData.messageId).to.be.a('string');
+                        chai.expect(messageData.readStatus).to.be.a('number');
+
+                        // 验证消息对象的属性值与预期值是否匹配
+                        chai.expect(messageData.conversationId).to.equal(conversationId);
+                        chai.expect(messageData.messageId).to.equal(messageId);
+                        chai.expect(messageData.readStatus).to.equal(1);
+
+                        socketAssertion = true;
+                    }
+                });
+            });
+
+            // 其他用户监听连接成功事件
+            otherSocket.on('connect', () => {
+                // 监听 WebSocket 推送消息
+                otherSocket.on('notifications', (message) => {
+                    if (message.type === 7) {
+                        // 验证消息对象的结构和属性
+                        const messageData = message.data;
+                        chai.expect(messageData).to.have.property('conversationId');
+                        chai.expect(messageData).to.have.property('messageId');
+                        chai.expect(messageData).to.have.property('content');
+                        chai.expect(messageData).to.have.property('sentTime');
+
+                        // 验证消息对象的属性值
+                        chai.expect(messageData.conversationId).to.be.a('string');
+                        chai.expect(messageData.messageId).to.be.a('string');
+                        chai.expect(messageData.content).to.be.a('string');
+                        chai.expect(messageData.sentTime).to.be.a('number');
+
+                        // 验证消息对象的属性值与预期值是否匹配
+                        chai.expect(messageData.conversationId).to.equal(conversationId);
+                        chai.expect(messageData.content).to.equal(lastMessageContent);
+
+                        // 设置最后一条消息的发送时间
+                        lastMessageTime = messageData.sentTime;
+
+                        // 设置消息id
+                        messageId = messageData.messageId;
+
+                        // 客户端推送标记消息已读消息到服务端
+                        otherSocket.emit('messages', {
+                            type: 5,
+                            data: {
+                                conversationId,
+                                messageId,
+                            },
+                        });
+
+                        return;
+                    }
+
+                    // 如果是已标记为已读的消息对象
+                    if (message.type === 8) {
+                        // 验证消息的结构和属性
+                        const messageData = message.data;
+
+                        // 验证消息对象的结构和属性
+                        chai.expect(messageData).to.have.property('conversationId');
+                        chai.expect(messageData).to.have.property('messageId');
+                        chai.expect(messageData).to.have.property('readStatus');
+
+                        // 验证消息对象的属性值
+                        chai.expect(messageData.conversationId).to.be.a('string');
+                        chai.expect(messageData.messageId).to.be.a('string');
+                        chai.expect(messageData.readStatus).to.be.a('number');
+
+                        // 验证消息对象的属性值与预期值是否匹配
+                        chai.expect(messageData.conversationId).to.equal(conversationId);
+                        chai.expect(messageData.messageId).to.equal(messageId);
+                        chai.expect(messageData.readStatus).to.equal(1);
+
+                        otherSocketAssertion = true;
+                    }
+                });
+            });
+
+            // 检查回调的断言状态
+            const intervalId = setInterval(() => {
+                if (socketAssertion && otherSocketAssertion) {
+                    clearInterval(intervalId);
+                    done();
+                }
+            }, 100);
+
+            // 推送创建聊天会话消息到服务端
+            socket.emit('messages', {
+                type: 0,
+                data: {
+                    opponentUserId: otherUser.id,
+                },
+            });
+        });
+    });
+
     afterEach(async () => {
         // 关闭 WebSocket 连接
         if (socket.connected) {
